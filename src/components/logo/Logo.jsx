833450f132d094c9e795b42093bf93d2b52764da
unknown <www.g.roshleva7@gmail.com>
2022-01-21 19:26:56 +0200

    Commit 1 logo

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,10 @@
+import styles from "./Logo.module.scss";
+
+export default function Logo({ type = "default" }) {
+  return (
+    <div>
+      {type === "default" && <img src="/images/logo.svg" />}
+      {type === "muted" && <img src="/images/logo-muted.svg" />}
+    </div>
+  );
+}
