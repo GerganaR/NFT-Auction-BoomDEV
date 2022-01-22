1f4eb6e070d0f32d688f8d986963568e471bbc94
unknown <www.g.roshleva7@gmail.com>
2022-01-22 12:02:43 +0200

    Commit 2 Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,66 @@
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Logo from "../logo/Logo.jsx";
+import Link from "../link/Link.jsx";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+import classNames from "classnames";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <div className={classNames(styles["header-container"])}>
+      <Grid
+        container
+        maxWidth="xl"
+        className={classNames(styles["grid-container"])}
+      >
+        <Grid item md={2} display={{ xs: "none", md: "flex" }}>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </Grid>
+        <Grid item xs={12} md={5} paddingX={{ xs: "10px", md: "0" }}>
+          <TextField
+            className={classNames(styles["search-bar"])}
+            fullWidth
+            placeholder="Find items, users and activities"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start" color="secondary">
+                  <SearchIcon className={classNames(styles["search-icon"])} />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Grid>
+        <Grid
+          item
+          container
+          className={classNames(styles["button-container"])}
+          xs={12}
+          md={5}
+          justifyContent={{ xs: "center", md: "flex-end" }}
+        >
+          <Link href="/">
+            <Button className={classNames(styles["button"])} variant="text">
+              Home
+            </Button>
+          </Link>
+          <Link href="/activity">
+            <Button className={classNames(styles["button"])} variant="text">
+              Activity
+            </Button>
+          </Link>
+          <Link href="/explore">
+            <Button variant="contained" size="large">
+              Explore
+            </Button>
+          </Link>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
