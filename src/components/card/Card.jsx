dabc1d477a4f733628d54b01969eb076230c807f
unknown <www.g.roshleva7@gmail.com>
2022-01-25 13:10:38 +0200

    Commited Card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,74 +1,70 @@
-import {
-  Card as MUICard,
-  CardContent,
-  CardMedia,
-  Container,
-  Box,
-  Chip,
-} from "@mui/material/";
 import classNames from "classnames";
 import styles from "./Card.module.scss";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Avatar from "../avatar/Avatar";
+import Grid from "@mui/material/Grid";
+import CardMUI from "@mui/material/Card";
+import Chip from "@mui/material/Chip";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardContent from "@mui/material/CardContent";
+import Container from "@mui/material/Container";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import millify from "millify";
 
 export default function Card({
-  name,
+  name = "",
   likes = 0,
-  mediaUrl,
-  owner,
-  price,
-  currency,
+  mediaUrl = "",
+  user,
+  price = "",
+  currency = "",
 }) {
   return (
-    <MUICard>
+    <CardMUI className={classNames(styles["card"])}>
+      <Container disableGutters className={classNames(styles["imgContainer"])}>
+        <Avatar {...user} />
+        <CardMedia
+          className={classNames(styles["media"])}
+          component="img"
+          image={mediaUrl}
+          alt="NFTImage"
+        ></CardMedia>
+      </Container>
       <CardContent className={classNames(styles["card-content"])}>
-        {owner && (
-          <Avatar verified={owner.verified} url={owner.avatar.url} size={33} />
-        )}
-        <Container disableGutters className={classNames(styles.imgContainer)}>
-          <CardMedia
-            component="img"
-            image={mediaUrl}
-            alt="card media"
-            className={classNames(styles.media)}
-          />
-        </Container>
-        <Grid container justifyContent="space-between" alignItems="center">
-          <Grid item>
-            <Typography
-              className={classNames(styles.title)}
-              sx={{ fontWeight: "900", marginBottom: "5px" }}
-            >
-              {name}
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item xs={9}>
+            <Typography className={classNames(styles["title"])}>
+              {name}Name NFT
             </Typography>
-            <Typography
-              className={classNames(styles.price)}
-              sx={{ fontWeight: "700", fontSize: "0.9rem" }}
-            >
-              {price} {currency}
+            <Typography className={classNames(styles["price"])}>
+              {price} {currency} Currency
             </Typography>
           </Grid>
-
-          <Chip
-            variant="outlined"
-            color="success"
-            size="small"
-            sx={{
-              height: "33px",
-              border: "2px solid",
-              fontWeight: "bold",
-              background: "rgba(36, 242, 94, 0.1)",
-            }}
-            icon={<FavoriteIcon />}
-            label={millify(likes)}
-            onClick={() => {}}
-            className={classNames(styles.likes)}
-          />
+          <Grid item xs={3}>
+            <Chip
+              className={classNames(styles["likes"])}
+              variant="outlined"
+              color="success"
+              size="small"
+              sx={{
+                height: "33px",
+                border: "2px solid",
+                fontWeight: "bold",
+                background: "rgba(36, 242, 94, 0.1)",
+              }}
+              icon={<FavoriteIcon />}
+              label={millify(likes)}
+              variant="outlined"
+            ></Chip>
+          </Grid>
         </Grid>
       </CardContent>
-    </MUICard>
+    </CardMUI>
   );
 }
