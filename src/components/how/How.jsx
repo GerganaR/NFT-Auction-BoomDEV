180bc905827d4fea4b990ebb757ce87016690105
unknown <www.g.roshleva7@gmail.com>
2022-02-08 12:11:26 +0200

    New changes

--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -1,12 +1,27 @@
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
 import styles from "./How.module.scss";
 import classNames from "classnames";
-import HowStep from "./HowStep.jsx";
-import { Container, Grid, Button } from "@mui/material";
+import HowStep from "./HowStep";
+import { Grid, Button } from "@mui/material";
 
 export default function How({
   description = "",
   title = "",
-  items = [],
+  items = [
+    {
+      title: "Test 1",
+      description: "Test 1 Desc",
+    },
+    {
+      title: "Test 1",
+      description: "Test 1 Desc",
+    },
+    {
+      title: "Test 1",
+      description: "Test 1 Desc",
+    },
+  ],
   link = "",
 }) {
   return (
@@ -14,8 +29,12 @@ export default function How({
       <Grid container spacing={2}>
         <Grid item xs={8}>
           <h1>{title}</h1>
-          <p> {description}</p>
-          <Button href={link} variant="contained">
+          <p>{description}</p>
+          <Button
+            href={link}
+            variant="contained"
+            className={classNames(styles["learn-more-btn"])}
+          >
             Learn more
           </Button>
         </Grid>
--- src/components/how/HowStep.jsx
+++ src/components/how/HowStep.jsx
@@ -1,16 +1,18 @@
+import React, { useState } from "react";
+import Container from "@mui/material/Container";
 import styles from "./HowStep.module.scss";
 import classNames from "classnames";
-import { Container, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 
-export default function Step({ number = 0, title = "", description = "" }) {
+export default function HowStep({ number = 0, title = "", description = "" }) {
   return (
     <Container className={classNames(styles.container)} maxWidth="xl">
       <Grid container spacing={2}>
         <Grid item xs={4}>
-          <p className={classNames(styles.number)}> {number++}</p>
+          <p className={classNames(styles.number)}>{number++}</p>
         </Grid>
         <Grid item xs={8}>
-          <h2>{title.toUpperCase()}</h2>
+          <h1>{title}</h1>
           <p>{description}</p>
         </Grid>
       </Grid>
